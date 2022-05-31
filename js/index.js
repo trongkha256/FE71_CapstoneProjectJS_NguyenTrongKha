e4f64d7c7e253b2eb7ad125fda90c4a3607b0701
trongkha256 <trongkha256@gmail.com>
2022-05-31 16:20:04 +0700

    Complete part 1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let productList = []
+let carts = []
 
 const fetchProduct = async () => {
     try {
@@ -8,30 +9,78 @@ const fetchProduct = async () => {
         });
         productList = mapProduct(res.data)
         renderProducts(productList);
-
     }
     catch (err) {
         console.log(err);
     }
 }
+
 const renderProducts = function (data) {
     data = data || ProductList;
     var dataHTML = ""
     for (var i = 0; i < data.length; i++) {
         dataHTML += `
-            <div class="col-3 text-center" heigh="1000px">
+            <div class="col-3 text-center">
                 <div class="img bg-secondary">
                     <img src=${data[i].img} width="100%" height="200px">
                 </div>
                 <div class="text">
                     <p style="font-size: 12px">${data[i].desc}</p>
                 </div>
-                <button class="btn btn-success text-center w-100 d-flex align-items-end">Cart</button>
+                <button class="btn btn-success text-center w-100 d-flex align-items-end" onclick="addToCarts(${data[i].id})">Cart</button>
             </div>
  
     `
     }
     document.getElementById('porfolio').innerHTML = dataHTML;
+}
+const renderCarts = (data) => {
+    data = data || carts
+    let sum = 0
+    for (var i = 0; i < data.length; i++) {
+        sum += parseInt(data[i].price) * parseInt(data[i].quantity)
+    }
+    // const sum = data.reduce((itemprev, item) => { return itemprev.quantity * itemprev.price + item.quantity * item.price }, 0)
+    // console.log(sum)
+    var dataHTML = "";
+    for (var i = 0; i < data.length; i++) {
+        dataHTML += `
+        <tr>
+            <td>
+            <img src=${data[i].img} width="100px" height="100px">
+            </td>
+            <td>
+            <p class="fs-5">${data[i].name}</p></td>
+            <td>${data[i].price}</td>
+            <td> <button id="btnDecrease" class='btn btn-success'
+             onclick="handleQuantity(${data[i].id},${-1})"
+            >-</button>
+        <span>${data[i].quantity}</span>
+        <button class='btn btn-success'onclick="handleQuantity(${data[i].id},${1})" >+</button></td>
+            <td>${data[i].quantity * data[i].price}</td>
+            <td><button class='btn btn-danger' onclick="handleDeleteCart(${data[i].id})" >Xóa</button></td>
+    </tr>
+    
+ 
+    `
+    }
+    dataHTML += `
+    <h1>Tổng tiền: ${sum}</h1>
+    <button class="btn btn-success class="text-end" onclick="handlePay()">Thanh toán</button>
+    `
+    document.getElementById('cart').innerHTML = dataHTML;
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].quantity === 1) {
+            document.getElementById('btnDecrease').disabled = true
+        }
+    }
+
+}
+const logic = (product) => {
+    if (product.quantity === 1) {
+        return true
+    }
+    return false
 }
 const mapProduct = (data) => {
     const results = data.map((item, i) => {
@@ -62,5 +111,46 @@ const handleSearch = () => {
     })
     renderProducts(temp)
 }
+const addToCarts = (productId) => {
+    const a = [...productList]
+    const index = carts.findIndex(
+        (product) => product.id == productId
+    )
+    if (index === -1) {
+        a.forEach((item) => {
+            if (item.id == productId) {
+                carts.push({ ...item, quantity: 1 });
+            }
+        })
+    }
+    else carts[index].quantity += 1
 
+    console.log(carts)
+    renderCarts(carts)
+}
+const handlePay = () => {
+    carts = [];
+    renderCarts(carts)
+}
+const handleQuantity = (productId, typical) => {
+
+    carts.forEach((item) => {
+        if (productId == item.id && typical === -1) {
+            item.quantity -= 1
+        }
+        else {
+            if (productId == item.id && typical === 1) {
+                item.quantity += 1
+            }
+        }
+    })
+    renderCarts(carts)
+}
+const handleDeleteCart = (productId) => {
+    temp = carts.filter((item) => {
+        return item.id != productId
+    })
+    carts = temp;
+    renderCarts(carts)
+}
 fetchProduct();
